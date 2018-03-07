0514278ddf2d1de468e5f7dca72057ef485ea909
Ethan Holman <ethanh@Ethans-MacBook-Air.local>
2018-03-07 15:39:03 -0800

    can now add items to cart

--- src/app/routed-modules/inventory/components/product-detail.component.ts
+++ src/app/routed-modules/inventory/components/product-detail.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute } from "@angular/router";
+import { ActivatedRoute, Router } from "@angular/router";
 import { Product } from "../../../core/models/product";
 import { ProductService } from "../../../core/services/product.service";
+import { CartService } from "../../../core/services/cart.service";
 
 @Component({
     templateUrl: 'product-detail.component.html',
@@ -11,17 +12,27 @@ export class ProductDetailComponent implements OnInit {
 
     loadedProduct = false;
     product: Product;
+    quantityToCart: number;
 
-    constructor(private _router: ActivatedRoute, private _productService: ProductService) {}
+    constructor(
+        private _route: ActivatedRoute,
+        private _productService: ProductService,
+        private _cartService: CartService,
+        private _router: Router) {}
 
     ngOnInit(): void {
-        this._router.params.subscribe(params => {
+        this._route.params.subscribe(params => {
             const id = params['id'];
             this._productService.getProductById(id).subscribe(data => {
                 this.product = data;
                 this.loadedProduct = true;
             });
         });
+    }
+
+    addToCart(): void {
+        this._cartService.addToCart(this.product, this.quantityToCart);
+        this._router.navigateByUrl("/checkout");
     }
 
 }
--- src/app/routed-modules/inventory/inventory.module.ts
+++ src/app/routed-modules/inventory/inventory.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { ProductListComponent } from './components/product-list.component';
 import { CommonModule } from '@angular/common';
 import { ProductDetailComponent } from './components/product-detail.component';
+import { FormsModule }   from '@angular/forms';
 
 @NgModule({
     imports: [
-        CommonModule
+        CommonModule,
+        FormsModule
     ],
     declarations: [
         ProductListComponent,
